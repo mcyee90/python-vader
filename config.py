3ba6c318df47de4b4e97899c14040406769c4947
mcyee90 <mcyee90@gmail.com>
2018-03-15 18:09:23 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+apiKeyGoogle = 'AIzaSyAGxBETHvM7Wbji3fzfHswzT0i6xCcHdGA'
+
+twitterConsumerKey = "SDrpjytN0WCRyOjMFWomGWMUD"
+twitterConsumerSecret = "19xZsMfMmX6I4Esdeimv1Ya3iWzLPRKBTSj9NpGk2rLhlacaL8"
+twitterAccessToken = "969399451289661440-5PSLNXiWrCAlu3jwiMZrTT0iMXsoTTD"
+twitterAccessTokenSecret = "JvI5XDmliwv8tUziNOKJjIP5sOOLcVDHU7R415ooX5wrs"
